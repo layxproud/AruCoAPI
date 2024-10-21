6a8ee1537a7ea8943c57e204e910aa5f90e7cf87
layxproud <layxproud@gmail.com>
2024-10-21 16:54:22 +0300

    init

---
+++ AruCoAPI.pro
@@ -0,0 +1,34 @@
+QT -= gui
+
+TEMPLATE = lib
+DEFINES += ARUCOAPI_LIBRARY
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    capturethread.cpp \
+    arucoapi.cpp \
+    yamlhandler.cpp
+
+HEADERS += \
+    TestLib_global.h \
+    capturethread.h \
+    arucoapi.h \
+    yamlhandler.h
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/third_party/opencv_mingw810/x64/mingw/lib/ -llibopencv_world4100
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/third_party/opencv_mingw810/x64/mingw/lib/ -llibopencv_world4100d
+else:unix: LIBS += -L$$PWD/third_party/opencv_mingw810/x64/mingw/lib/ -llibopencv_world4100.dll
+
+INCLUDEPATH += $$PWD/third_party/opencv_mingw810/include
+DEPENDPATH += $$PWD/third_party/opencv_mingw810/include
+
+# Default rules for deployment.
+unix {
+    target.path = /usr/lib
+}
+!isEmpty(target.path): INSTALLS += target
---
+++ arucoapi.cpp
@@ -0,0 +1,90 @@
+#include "arucoapi.h"
+#include <QDebug>
+
+AruCoAPI::AruCoAPI(QObject *parent)
+    : QObject{parent}
+    , yamlHandler(new YamlHandler(this))
+    , captureThread(new CaptureThread(this))
+{
+    connect(captureThread, &CaptureThread::frameReady, this, &AruCoAPI::frameReady);
+    connect(captureThread, &CaptureThread::distanceCalculated, this, &AruCoAPI::distanceCalculated);
+    connect(captureThread, &CaptureThread::centerFound, this, &AruCoAPI::centerFound);
+}
+
+AruCoAPI::~AruCoAPI()
+{
+    stopThread(captureThread);
+}
+
+void AruCoAPI::init()
+{
+    CalibrationParams calibrationParams;
+    if (!yamlHandler->loadCalibrationParameters("calibration.yml", calibrationParams)) {
+        emit taskFinished(false, tr("Не обнаружен файл калибровки. Сначала откалибруйте камеру!"));
+        return;
+    }
+    captureThread->setCalibrationParams(calibrationParams);
+    captureThread->setYamlHandler(yamlHandler);
+    startThread(captureThread);
+    qDebug() << "THREAD STARTED";
+}
+
+void AruCoAPI::startThread(QThread *thread)
+{
+    if (thread && !thread->isRunning()) {
+        thread->start();
+    }
+}
+
+void AruCoAPI::stopThread(QThread *thread)
+{
+    if (thread && thread->isRunning()) {
+        CaptureThread *capture = dynamic_cast<CaptureThread *>(thread);
+        if (capture) {
+            capture->stop();
+            capture->wait();
+            return;
+        }
+
+        thread->quit();
+        thread->wait();
+    }
+}
+
+void AruCoAPI::startMarkerDetectionTask()
+{
+    if (!captureThread->getMarkerDetectionStatus()) {
+        captureThread->setDistanceCalculatingStatus(false);
+        captureThread->setCenterFindingStatus(false);
+        captureThread->setMarkerDetectingStatus(true);
+        emit taskChanged(tr("Ведется поиск маркеров"));
+    }
+}
+
+void AruCoAPI::startDistanceCalculationTask()
+{
+    if (!captureThread->getDistanceCalculatingStatus()) {
+        captureThread->setCenterFindingStatus(false);
+        captureThread->setMarkerDetectingStatus(false);
+        captureThread->setDistanceCalculatingStatus(true);
+        emit taskChanged(tr("Ведется измерение расстояний"));
+    }
+}
+
+void AruCoAPI::startCenterFindingTask()
+{
+    if (!captureThread->getCenterFindingStatus()) {
+        captureThread->setDistanceCalculatingStatus(false);
+        captureThread->setMarkerDetectingStatus(false);
+        captureThread->setCenterFindingStatus(true);
+        emit taskChanged(tr("Ведется поиск центра"));
+    }
+}
+
+void AruCoAPI::cancelOperations()
+{
+    captureThread->setDistanceCalculatingStatus(false);
+    captureThread->setCenterFindingStatus(false);
+    captureThread->setMarkerDetectingStatus(false);
+    emit taskChanged(tr("Нет задачи"));
+}
